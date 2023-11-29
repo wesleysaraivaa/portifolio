536446e7294209e38701649e337ce928e27a38fa
wesleysaraivaa <wesley-saraiva@outlook.com>
2023-11-28 22:52:59 -0300

    (FEAT: concluido a section about)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 //----------------ABOUT-TAB-----------------
+//Elemento ja abre aberto
 document.getElementById("defaultOpen").click();
 
 function openTab(evt, Linkname) {
@@ -17,3 +18,36 @@ function openTab(evt, Linkname) {
     document.getElementById(Linkname).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+//----------------ABOUT-EXPANDER-----------------
+function createExpander(container) {
+    var elem;
+
+    elem = document.createElement('div');
+    elem.className = 'expander show';
+    elem.innerHTML = 'Mais';
+
+    elem.addEventListener('click', function () {
+        // se o conteúdo estiver ocultado, altera o height
+        // para que se adapte ao tamanho do conteúdo.
+        // Se não, apenas retorna ao tamanho original (350px).
+        if (elem.className === 'expander show') {
+            elem.innerHTML = 'Ocultar';
+            elem.className = 'expander hide';
+
+            container.style.height = 'auto';
+        } else {
+            elem.innerHTML = 'Mais';
+            elem.className = 'expander show';
+
+            container.style.height = '350px';
+        }
+    });
+
+    container.appendChild(elem);
+}
+
+// aplicamos em todos os elementos .expand na página.
+document.querySelectorAll('.expand').forEach(function (elem) {
+    createExpander(elem);
+});
